e0d7109e6eb654cb146b8c010ca5dfe561a1bfc5
Thomas Stringer <thstring@microsoft.com>
2021-11-07 13:13:42 -0500

    add describe for pods and all events

--- dump_pod_logs.sh
+++ dump_pod_logs.sh
@@ -7,9 +7,12 @@ EXTENSION="log"
 echo "Using output dir $OUTPUT_DIR"
 mkdir "$OUTPUT_DIR"
 
+# Get all pod logs and describe
 kubectl get po -A --no-headers | while read -r line; do
     NAMESPACE=$(echo "$line" | awk '{print $1}')
     POD_NAME=$(echo "$line" | awk '{print $2}')
+    FILENAME="${OUTPUT_DIR}/${NAMESPACE}.${POD_NAME}.describe"
+    kubectl describe pod -n "$NAMESPACE" "$POD_NAME" > "$FILENAME"
     for CONTAINER in $(kubectl get po -n "$NAMESPACE" "$POD_NAME" -o jsonpath="{.spec.containers[*].name}"); do
         FILENAME_PREFIX="${OUTPUT_DIR}/${NAMESPACE}.${POD_NAME}.${CONTAINER}"
         FILENAME="${FILENAME_PREFIX}.current.${EXTENSION}"
@@ -20,6 +23,10 @@ kubectl get po -A --no-headers | while read -r line; do
         kubectl logs -p -n "$NAMESPACE" "$POD_NAME" "$CONTAINER" > "$FILENAME" 2> /dev/null
     done
 done
+
+# Dump all events
+FILENAME="${OUTPUT_DIR}/events.log"
+kubectl get events -A > "$FILENAME"
 
 CWD=$(pwd)
 cd $ROOT_OUTPUT_DIR || exit 1
